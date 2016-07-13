9b6d573a40f97d718e7d66b6f094668e38f6768b
Christoph Anderson <cwolfanderson@gmail.com>
2016-07-13 13:37:09 -0700

    Add conditional logic examples

---
+++ conditional_logic.js/conditional_logic.js
@@ -0,0 +1,6 @@
+http://jsbin.com/demema/edit?js,console
+http://jsbin.com/bexoha/edit?js,console
+http://jsbin.com/zeviji/edit?js,console
+http://jsbin.com/fogika/1/edit?js,console
+http://jsbin.com/cofajuz/1/edit?js,console
+http://jsbin.com/fileda/edit?js,console
